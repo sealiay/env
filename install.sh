d9b02d681c18e0d1474eb9306d75e4826ea1c736
Chao Zhang <chao.zhang@hulu.com>
2017-10-11 11:17:40 +0800

    use http git

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ cp tmux.conf ~/.tmux.conf
 cp vimrc ~/.vimrc
 
 rm -rf ~/.oh-my-zsh
-git clone --depth=1 git@github.com:robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 rm -rf ~/.vim
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/undodir
-git clone --depth=1 git@github.com:gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim +PluginInstall +qall
 # ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
